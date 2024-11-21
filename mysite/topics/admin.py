80c6207057823187b9d6b789b39a9db25531720e
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2024-11-21 15:40:35 +0000

    feat: Register models on admin page
    - Registered current models on the Django admin page to enable simple value editing for testing purposes

--- mysite/topics/admin.py
+++ mysite/topics/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Topic, Comment, Profile
 
-# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Comment)
+admin.site.register(Profile)
