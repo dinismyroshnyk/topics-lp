384f0e81102855b5628d808a420693032dd9a5b3
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2024-11-21 11:48:57 +0000

    chore: Add nix shell
    - Added a `shell.nix` to the root of the project

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+    pythonEnv = pkgs.python3.withPackages (ps: with ps; [ pip ]);
+in
+pkgs.mkShell {
+    buildInputs = [
+        pythonEnv
+        pkgs.tree
+    ];
+
+    shellHook = ''
+        export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+        python3 -m venv .venv
+        source .venv/bin/activate
+
+        pip install django
+
+        alias cls='clear'
+        alias validate='django-admin --version'
+        alias new-project='django-admin startproject '
+        alias startapp='python manage.py startapp '
+        alias makemigrations='python manage.py makemigrations '
+        alias sqlmigrate='python manage.py sqlmigrate topics 0001'
+        alias migrate='python manage.py migrate'
+        alias createsuperuser='python manage.py createsuperuser'
+        alias runserver='python manage.py runserver'
+        alias run='echo "Nothing to run :("'
+    '';
+}
